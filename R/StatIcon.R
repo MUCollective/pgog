28e1b7176bf0104349423ab2793d7d8f78c8c977
Xiaoying Pu <xp002@bucknell.edu>
2018-11-25 13:38:18 -0500

    computes w and h of array

--- R/StatIcon.R
+++ R/StatIcon.R
@@ -1,17 +1,28 @@
-StatIcon <- ggproto("StatIcon", Stat,
-                     compute_group = function(data, scales) {
-                       browser()
-                       data
-                     },
+StatIcon <- ggproto(
+  "StatIcon", ggplot2::Stat,
 
-                     required_aes = c("x")
+  setup_params = function(data, params){
+    N <- nrow(data)
+    w_h <- factor_w_h(N)
+    params$icon_array_w <- w_h$w
+    params$icon_array_h <- w_h$h
+    params
+    browser()
+  },
+
+  compute_panel = function(data, scales, params) {
+    browser()
+    data
+  },
+
+  required_aes = c("x")
 )
 
 
 stat_icon <- function(mapping = NULL, data = NULL, geom = "point", # TODO
                        position = "identity", na.rm = FALSE, show.legend = NA,
                        inherit.aes = TRUE, ...) {
-  browser()
+  # browser()
   layer(
     stat = StatIcon, data = data, mapping = mapping, geom = geom,
     position = position, show.legend = show.legend, inherit.aes = inherit.aes,
---
+++ R/util.R
@@ -0,0 +1,8 @@
+factor_w_h <- function(n){
+  factor_test <- as.integer(sqrt(n))
+  while (n %% factor_test != 0) {
+    factor_test <- factor_test - 1
+  }
+  data.frame(w = max(factor_test, n/factor_test),
+             h = min(factor_test, n/factor_test))
+}
