33b92e8386e1fc2f8ca969bc0828270ad1c7973c
Xiaoying Pu <xp002@bucknell.edu>
2018-12-14 12:23:04 -0500

    oh well it's currently the bee swarm

---
+++ R/position-icon.R
@@ -0,0 +1,47 @@
+PositionIcon <- ggproto("PositionIcon", ggplot2:::Position,
+                    required_aes = c("x", "y"),
+
+                    # compute_layer = function(self, data, params, layout) {
+                    #   dapply(data, "PANEL", function(data) {
+                    #     if (empty(data)) return(new_data_frame())
+                    #
+                    #     scales <- layout$get_scales(data$PANEL[1])
+                    #     self$compute_panel(data = data, params = params, scales = scales)
+                    #   })
+                    # },
+
+                    compute_panel = function(self, data, params, scales) {
+                      # browser()
+
+                      # calculate some offsets
+                      trans_x <- function(xx) {
+                        new_x <- vipor::offsetX(
+                          data[, 'x'],
+                          xx
+                        )
+                        return(new_x + xx)
+                      }
+                      transform_position(data, trans_x, trans_x)
+                    }
+)
+
+
+position_icon <- function (
+  width = NULL,
+  varwidth = FALSE,
+  bandwidth=.5,
+  nbins=NULL,
+  method='icon',
+  groupOnX=NULL,
+  dodge.width=0){
+  ggplot2::ggproto(
+    NULL,
+    PositionIcon,
+    width = width,
+    varwidth = varwidth,
+    bandwidth=bandwidth,
+    nbins=nbins,
+    method=method,
+    groupOnX=groupOnX,
+    dodge.width=dodge.width)
+}
