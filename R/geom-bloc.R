58136d7130fbc34d2c362c5d33ad2249f3200ad5
Xiaoying Pu <xp002@bucknell.edu>
2018-12-23 14:50:55 -0500

    width=P(A), but x=A, width = P(A) is broken

--- R/geom-bloc.R
+++ R/geom-bloc.R
@@ -13,31 +13,37 @@ geom_bloc <- function(mapping = NULL, data = NULL,
                      inherit.aes = TRUE) {
 
   # pick layer based on aesthetics combo
-  aes_names <- names(enexpr(mapping))
+  aes_names <- names(mapping)
 
-  # if ("x" %in% aes_names &
-  #     !("y" %in% aes_names)){
-  #   # geom_histogram
-  #   ggplot2::layer(
-  #     data = data,
-  #     mapping = mapping,
-  #     stat = stat,
-  #     geom = GeomBar,
-  #     position = position,
-  #     show.legend = show.legend,
-  #     inherit.aes = inherit.aes,
-  #     params = list(
-  #       binwidth = binwidth,
-  #       bins = bins,
-  #       na.rm = na.rm,
-  #       pad = FALSE,
-  #       ...
-  #     ))
-  # } else if (!("x" %in% aes_names ) & !("y" %in% aes_names))  {
+  if ("x" %in% aes_names &
+      !("y" %in% aes_names)){
+    # geom_histogram
+    ggplot2::layer(
+      data = data,
+      mapping = mapping,
+      stat = stat,
+      geom = GeomBar,
+      position = position,
+      show.legend = show.legend,
+      inherit.aes = inherit.aes,
+      params = list(
+        binwidth = binwidth,
+        bins = bins,
+        na.rm = na.rm,
+        pad = FALSE,
+        ...
+      ))
+  } else if (!("x" %in% aes_names ) & !("y" %in% aes_names))  {
     # mosaic plot
-    # TODO: swap out the width to x
-      # browser()
-      geom_mosaic(mapping=mapping)
-  # }
+      # P(A) -> product(A)
+      width_quosure <- mapping[["width"]]
+      PA_expr <- quo_get_expr(width_quosure)
+      PA_expr[[1]] <- expr(product)
+      mapping$width <- quo_set_expr(mapping[["width"]], PA_expr)
 
+      # Replace width with x
+      replaced <- replace(names(mapping), match("width", aes_names), "x")
+      names(mapping) <- replaced
+      geom_mosaic(mapping=mapping)
+  }
 }
