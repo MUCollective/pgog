dd74a0968d6075b6b28fa8113cbf52743f3c3080
Xiaoying Pu <xp002@bucknell.edu>
2018-12-24 09:55:52 -0500

    wip making conditional var as fill var

--- R/geom-bloc.R
+++ R/geom-bloc.R
@@ -3,7 +3,7 @@
 #' @importFrom ggmosaic product
 #' @importFrom rlang eval_tidy
 geom_bloc <- function(mapping = NULL, data = NULL,
-                     stat = "bin",
+                     stat = NULL,
                      position = "stack",
                      ...,
                      binwidth = NULL,
@@ -15,13 +15,34 @@ geom_bloc <- function(mapping = NULL, data = NULL,
   # pick layer based on aesthetics combo
   aes_names <- names(mapping)
 
+  browser()
+  # generate fill based on conditional P(A|B)
+  if (is.null(mapping$fill)){
+
+    # extract what P() is conditioned on
+
+    fill_expr <- expr(happy)
+    fill_env <- quo_get_env(mapping[[1]])
+    mapping$fill <- new_quosure(fill_expr, env = fill_env)
+  }
+
+
+
   if ("x" %in% aes_names & "height" %in% aes_names){
-
-    # check for the discrete flag
-    # browser()
-
-    if (grepl("x = ~discrete", quo_text(mapping))){
+    if (grepl("x = ~discrete", quo_text(mapping)) &
+        grepl("height", quo_text(mapping))){
       # geom_bar
+
+      height_quosure <- mapping[["height"]]
+      mapping[["height"]] <- quo_set_expr(mapping[["height"]], expr((..count..)/sum(..count..)))
+
+      replaced <- replace(names(mapping), match("height", aes_names), "y")
+      names(mapping) <- replaced
+
+      if (is.null(stat)){
+        stat <- "count"
+      }
+
       geom_bar(mapping = mapping,
                stat = stat,
                position = position,
@@ -31,9 +52,8 @@ geom_bloc <- function(mapping = NULL, data = NULL,
                inherit.aes = inherit.aes)
 
     } else {
-      print("geom_histogram")
       # geom_histogram
-      # browser()
+
       # height -> y
       # P(.) -> stat(width*density)
       height_quosure <- mapping[["height"]]
@@ -42,6 +62,13 @@ geom_bloc <- function(mapping = NULL, data = NULL,
       replaced <- replace(names(mapping), match("height", aes_names), "y")
       names(mapping) <- replaced
 
+      if (is.null(binwidth)){
+        binwidth = 1
+      }
+
+      if (is.null(stat)){
+        stat = "bin"
+      }
 
       geom_histogram(mapping = mapping,
                      stat = stat,
@@ -52,10 +79,10 @@ geom_bloc <- function(mapping = NULL, data = NULL,
                      show.legend = show.legend,
                      inherit.aes = inherit.aes)
     }
-
   } else if (!("x" %in% aes_names ) & !("y" %in% aes_names))  {
     # mosaic plot
     # P(A) -> product(A)
+
     width_quosure <- mapping[["width"]]
     PA_expr <- quo_get_expr(width_quosure)
     PA_expr[[1]] <- expr(product)
@@ -66,6 +93,7 @@ geom_bloc <- function(mapping = NULL, data = NULL,
     names(mapping) <- replaced
 
     # yay
+
     geom_mosaic(mapping=mapping)
   }
 }
