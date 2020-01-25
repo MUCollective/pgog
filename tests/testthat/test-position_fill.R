c0ee25ea2d9b2b42721f7556fa1f15014d4fe734
Xiaoying Pu <xp002@bucknell.edu>
2020-01-24 20:43:15 -0500

    skip vdiffr in tests

--- tests/testthat/test-position_fill.R
+++ tests/testthat/test-position_fill.R
@@ -1,8 +1,14 @@
 library(ggplot2)
 
+
 common_bw <- 1.5
 
 test_that("P(A,B), equiv to using position=fill", {
+
+  skip_if_not_installed("vdiffr")
+  skip_if_not_installed("svglite")
+
+
   base_ggplot <- ggplot(mtcars) + geom_density(aes(x = mpg, fill = factor(cyl)), position = "fill", bw=common_bw)
   vdiffr::expect_doppelganger("ggplot2 fill", base_ggplot)
 
