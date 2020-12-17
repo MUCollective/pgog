7d2f032d44b76fefe07f7f190942f96a2251ef01
DanielWang2029 <71171076+DanielWang2029@users.noreply.github.com>
2020-12-17 23:29:00 +0800

    Update test-dw.R
    Changed variable names in test

--- tests/testthat/test-dw.R
+++ tests/testthat/test-dw.R
@@ -3,13 +3,13 @@ library(rlang)
 library(tidyverse)
 library(vdiffr)
 
-test_that("plots have known output", {
+test_that("test for the order of the plot", {
   # disp_hist_base <- function() hist(mtcars$disp)
   # expect_doppelganger("disp-histogram-base", disp_hist_base)
 
   # disp_hist_ggplot <- ggplot(mtcars, aes(disp)) + geom_histogram()
   # expect_doppelganger("disp-histogram-ggplot", disp_hist_ggplot)
 
-  mpg_plot <- ggplot(mtcars) + geom_bloc(aes(x = c(mpg), height = c(P(mpg | cyl)), y = c(cyl), fill = cyl))
-  expect_doppelganger("mpg_plot", mpg_plot)
+  color_order_plot <- ggplot(mtcars) + geom_bloc(aes(x = c(mpg), height = c(P(mpg | cyl)), y = c(cyl), fill = cyl))
+  expect_doppelganger("mpg_plot", color_order_plot)
 })
